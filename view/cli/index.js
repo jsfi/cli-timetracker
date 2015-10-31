9b6e8702a7684efb0b93affabb1f3939e54d9633
Martin Sachse <martin.sachse+jsfi@gmail.com>
2015-10-31 21:43:57 +0100

    added view to eslint-tests

--- view/cli/index.js
+++ view/cli/index.js
@@ -13,27 +13,27 @@ const moment = require('moment');
 
 module.exports = function(config) {
     return function(tasks) {
-        let output = false;
+        let emptyLine = false;
 
         tasks.forEach(function(dayTasks) {
             if (!dayTasks.length) {
                 return;
             }
 
-            if (output) {
-                console.log();
+            if (emptyLine) {
+                console.log(); // eslint-disable-line no-console
             }
 
-            console.log(dayTasks.date.format(config.i18n.dayFormat));
+            console.log(dayTasks.date.format(config.i18n.dayFormat)); // eslint-disable-line no-console
             dayTasks.forEach(function(task) {
-                console.log(util.format(config.i18n.taskFormat, moment(task.time, 'X').format(config.i18n.taskTimeFormat), task.task));
+                console.log(util.format(config.i18n.taskFormat, moment(task.time, 'X').format(config.i18n.taskTimeFormat), task.task)); // eslint-disable-line no-console
             });
 
-            output = true;
+            emptyLine = true;
         });
 
-        if (!output) {
-            console.log(config.i18n.noTasksInPeriod);
+        if (!emptyLine) {
+            console.log(config.i18n.noTasksInPeriod); // eslint-disable-line no-console
         }
 
         return tasks;
